4bd73f68d7eef2776770ecd169556c31d39c21cd
Warren Roberson <warrenroberson@Warrens-MacBook-Pro.local>
2017-12-07 22:30:44 +0200

    Initial Commit

---
+++ Fidelity/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fidelity
+//
+//  Created by Warren Roberson on 2017/12/07.
+//  Copyright © 2017 Warren Roberson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
